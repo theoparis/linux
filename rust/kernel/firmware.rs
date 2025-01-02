c8cd08654cf14e0dbc8e7852182804ed02227405
Theo Paris <theo@tinted.dev>
2025-01-01 17:52:32 -0800

    fix(rust): fix pointer types
    This commit fixes pointer types, which may be different on various
    architectures. The fix is to use core::ffi::c_char instead of u8 or i8.

--- rust/kernel/firmware.rs
+++ rust/kernel/firmware.rs
@@ -5,6 +5,7 @@
 //! C header: [`include/linux/firmware.h`](srctree/include/linux/firmware.h)
 
 use crate::{bindings, device::Device, error::Error, error::Result, str::CStr};
+use core::ffi::c_char;
 use core::ptr::NonNull;
 
 /// # Invariants
@@ -12,7 +13,11 @@ use core::ptr::NonNull;
 /// One of the following: `bindings::request_firmware`, `bindings::firmware_request_nowarn`,
 /// `bindings::firmware_request_platform`, `bindings::request_firmware_direct`.
 struct FwFunc(
-    unsafe extern "C" fn(*mut *const bindings::firmware, *const i8, *mut bindings::device) -> i32,
+    unsafe extern "C" fn(
+        *mut *const bindings::firmware,
+        *const c_char,
+        *mut bindings::device,
+    ) -> i32,
 );
 
 impl FwFunc {
